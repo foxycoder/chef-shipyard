aaae46cb31d4bc6b512f65c6f154a817a1b48958
Wouter de Vos <wouter@springest.com>
2014-07-02 11:42:10 +0200

    Broadcast eth2 ip instead of public one.

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -14,7 +14,7 @@ docker_image 'shipyard/agent'
 docker_container 'shipyard/agent' do
   detach true
   tty true
-  env ["URL=#{node['shipyard']['url']}", "IP=#{node['network']['interfaces']['eth0']['addresses'].keys[1]}"]
+  env ["URL=#{node['shipyard']['url']}", "IP=#{node['network']['interfaces']['eth2']['addresses'].keys[1]}"]
   port '4500:4500'
   container_name 'shipyard_agent'
   volume "/var/run/docker.sock:/docker.sock"
